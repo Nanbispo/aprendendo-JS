95f70dc474cd03cc6aae2b57b4e94f218ffa2c43
Renan Bispo <renanjesusbispo@gmail.com>
2025-09-11 21:23:52 -0300

    diferenciação do web para servidor

---
+++ Aula4/index.js
@@ -0,0 +1,4 @@
+//Comentarios em JavaScript
+
+console.log("Olá Mundo!") //Aqui temos um comentário no final da linha
+console.log("Linha 1") //Tudo que se encontra após as barras é ignorado pelo interpretador
