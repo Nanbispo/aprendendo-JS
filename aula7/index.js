e260250f73d467dbe521bcbcdbfe8df854377720
Renan Bispo <renanjesusbispo@gmail.com>
2025-09-20 17:18:55 -0300

    Aprendendo sobre constantes (diferença entre const e let)

---
+++ aula7/index.js
@@ -0,0 +1,8 @@
+const numeroUm = 12
+const numeroDois = 154
+const result = numeroUm * numeroDois
+
+const resultDuplicate = result * 2
+console.log(resultDuplicate)
+
+console.log(typeof(numeroUm)) 
