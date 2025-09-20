476baaf6ede23fb245f23f47b050109845c26e1d
Renan Bispo <renanjesusbispo@gmail.com>
2025-09-20 18:58:22 -0300

    conhecendo e entendendo sobre tipos de dados em variaveis de JS

---
+++ aula9/index.js
@@ -0,0 +1,14 @@
+// AS VARIAVEIS DO TIPO TEXTO PODE SER DECLARADA DE DIVERSAS FORMAS:
+const nome = 'Otavio' //STRING
+const nome2 = "Otavio" //STRING
+const nome3 = `Otavio` //STRING
+
+const num1 = 10 //number
+const num2 = 20.52 //number (numero de ponto flutuante)
+
+let nomeAluno; //undefined = não aponta pra local nenhum na memmoeria
+const sobrenomeAluno = null; //Nulo = Não aponta para nenhum local na memoria (diferente de UNDEFINED)
+
+
+const alunoAprovado = true; //O tipo de dado booleano contem apenas dois valores (VERDADEIRO OU FALSO), booleano é um fator crucial, por ser um valor lógico
+
