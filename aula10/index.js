9b7d29583440f2e1c3c5eb718daa511072fa6a1a
Renan Bispo <renanjesusbispo@gmail.com>
2025-09-20 19:32:45 -0300

    Entendendo sobre operadores aritimeticos

---
+++ aula10/index.js
@@ -0,0 +1,64 @@
+//Operadores aritmeticos: + - / * ** %
+// O sinal + é um operador de multipla fução, por fazer a função matematica de soma e a função lógica de concatenação!
+
+
+//EXEMPLO DE CONCATENAÇÃO
+const nome = 'RENAN'
+const sobrenome = 'BISPO'
+console.log(nome + sobrenome)
+
+//EXEMPLO DE SOMA 
+const soma1 = 12
+const soma2 =  10
+console.log(soma1 + soma2)
+
+//EXEMPLO DE SUBTRAÇÃO
+const sub1 = 12
+const sub2 =  10
+console.log(sub1 - sub2)
+
+//EXEMPLO DE DIVISÃO
+const div1 = 12
+const div2 =  10
+console.log(div1 / div2)
+
+//EXEMPLO DE MULTIPLICAÇÃO
+const multi1 = 12
+const multi2 =  10
+console.log(multi1 * multi2)
+
+//EXEMPLO DE POTENCIAÇÃO
+const pot1 = 12
+const pot2 =  10
+console.log(pot1 ** pot2)
+
+//EXEMPLO DE RESTO DE DIVISÃO
+const rest1 = 12
+const rest2 =  10
+console.log(rest1 % rest2)
+
+//OPERADOR DE INCREMENTAÇÃO (acrescenta 1 somado ao valor anterior)
+let contador = 1
+contador++
+console.log(contador)
+
+//OPERADOR DE DECREMENTO (diminue 1 somado ao valor anterior)
+let decremento = 1
+decremento--
+console.log(decremento)
+
+
+//OPERADORES DE ATRIBUIÇÃO += -= /= *=
+let cont = 12
+cont += 2
+console.log(cont)
+
+cont -= 2
+console.log(cont)
+
+cont /= 2
+console.log(cont)
+
+cont *= 2
+console.log(cont)
+
